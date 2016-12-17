a6bdbc2775c7b8f262981d36d28634780c35f756
flakesns <flakesns@gmail.com>
2016-12-18 03:58:58 +0800

    url

--- public/js/services/diceService.js
+++ public/js/services/diceService.js
@@ -6,13 +6,10 @@ angular.module('diceService', [])
     	
     	rolldice : function(param) {
     		
-    		myurl = window.location;
-    		alert(myurl)
-    		
     		return $http({
                 method: 'POST',
                 //url: '/dice/public/',
-                url: myurl,
+                url: '/',
                 headers: { 'Content-Type' : 'application/x-www-form-urlencoded' },
                 data: $.param({round:param})
             });
